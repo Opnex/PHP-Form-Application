9113ada66741fad53ffb97ffef01b4ef14740a87
Opeyemi <thomasopeyemi1@gmail.com>
2025-06-24 08:34:33 +0100

    email input initialization

--- process.php
+++ process.php
@@ -101,7 +101,7 @@
     </style>
 </head>
 <body>
-    
+
     <div class="container">
         <h1>Form Submission Result</h1>
         <div class="result">
@@ -115,7 +115,22 @@
                 $favorite_color = htmlspecialchars($_POST['favorite_color']);
                   
                 echo '<p class="success">Hello ' . $name . ', you are ' . $age . ' years old, and your favorite color is ' . $favorite_color . '.</p>';
-            }
+
+            //     $to = "thomasopeyemi1@gmail.com"; 
+            //     $subject = "New Form Submission";
+            //     $message = "A new user has submitted the form:\n\n"
+            //     . "Name: $name\n"
+            //     . "Age: $age\n"
+            //     . "Favorite Color: $favorite_color\n";
+            //     $headers = "From: thomasopeyemi2@gmail.com";
+
+            
+            // if (mail($to, $subject, $message, $headers)) {
+            //     echo '<p class="success">Hello ' . $name . ', you are ' . $age . ' years old, and your favorite color is ' . $favorite_color . '.<br>A confirmation email has been sent to the admin.</p>';
+            // } else {
+            //     echo '<p class="error">Form submitted, but email could not be sent.</p>';
+            // }
+        }
             ?>
 
         </div>
